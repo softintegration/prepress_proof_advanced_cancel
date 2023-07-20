34093b5182d0d857e29bbbf7e9860fa610409da6
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-07-20 12:48:15 +0100

    Prepress proof:Add the wizard of the cancelation with the motif

--- models/prepress_proof.py
+++ models/prepress_proof.py
@@ -11,7 +11,16 @@ class PrepressProof(models.Model):
     state = fields.Selection(selection_add=[('pending_cancel', 'Pending cancel')],ondelete={'pending_cancel': 'set default'})
 
 
+    def action_start_cancel_with_motif(self):
+        return self.with_context(model=self._name,
+                                 model_ids=self.ids,
+                                 method='action_start_cancel',
+                                 default_display_cancel_date=False)._action_cancel_motif_wizard()
+
     def action_start_cancel(self):
+        # we have to remove the Cancel date because it will be filled by the standard wizard ,
+        # in our case we have not achieve yet the cancelation
+        self.write({'cancel_date':False})
         return self._action_start_cancel()
 
     def _action_start_cancel(self):
